ea2354251969f5a14b21f50d7575ca508a7a0d33
Benjozorrk <benjozorkfr@gmail.com>
2018-11-25 18:10:37 -0500

    simplified abstractRenderer

--- app/src/ui/renderers/AbstractRenderer.ts
+++ app/src/ui/renderers/AbstractRenderer.ts
@@ -1,13 +1,11 @@
 import {RedditContent} from "snoowrap";
 
-export abstract class AbstractRenderer<C extends RedditContent<C>, E extends HTMLElement> {
+export class AbstractRenderer<C extends RedditContent<C>, E extends HTMLElement> {
 
     returnElement: E;
 
-    abstract renderElement(data: RedditContent<C>): E;
-
     protected applyStylesheet(relativePathToCss: string) {
-        let styles = require('./PostStylesheet.css').toString();
+        let styles = require('./postHeader/PostHeaderStylesheet.css').toString();
         console.log(styles);
     }
 
