e651c5973c7a70499328c0eb82a8e13a8157c672
Benjozorrk <benjozorkfr@gmail.com>
2018-11-25 21:56:35 -0500

    removed dead code from PostHeaderRenderer.ts

--- app/src/ui/renderers/postHeader/PostHeaderRenderer.ts
+++ app/src/ui/renderers/postHeader/PostHeaderRenderer.ts
@@ -1,6 +1,6 @@
 import {Submission} from 'snoowrap';
 
-import {AbstractRenderer} from "../AbstractRenderer";
+import {AbstractRenderer}      from "../AbstractRenderer";
 
 import * as postHeaderTemplate from './PostHeaderTemplate.dot';
 
@@ -28,104 +28,6 @@ export class PostHeaderRenderer extends AbstractRenderer<Submission, HTMLDivElem
 
         return returnElement.firstElementChild
 
-        /*this.returnElement = <HTMLDivElement>(document.createElement('div'));
-        this.returnElement.className = 'post-header';
-
-        let headerContent = <HTMLDivElement>(document.createElement('div'))
-        headerContent.className = 'post-header-content';
-
-        this.returnElement.appendChild(this.renderVotes(post.score, true, false));
-
-        headerContent.appendChild(this.renderFirstLine(post))
-        headerContent.appendChild(this.renderSecondLine(post))
-
-        this.returnElement.appendChild(headerContent)
-
-        this.applyStylesheet('./PostStylesheet.css')
-
-        return this.returnElement;*/
-
-        return <HTMLDivElement>(document.createElement('div'))
-
-    }
-
-    private renderFirstLine(post: Submission): HTMLElement {
-
-        let returnElement = <HTMLDivElement>(document.createElement('div'));
-        returnElement.className = 'post-header-line1';
-
-        let clockImage = <HTMLImageElement>(document.createElement('img'));
-        clockImage.className = 'clock-icon';
-
-        let timeSpan = <HTMLSpanElement>(document.createElement('span'));
-        timeSpan.className = 'post-time';
-        timeSpan.innerText = post.created_utc.toString();
-
-        returnElement.appendChild(clockImage);
-        returnElement.appendChild(timeSpan);
-
-        return returnElement
-
-    }
-
-    private renderSecondLine(post: Submission): HTMLElement {
-
-        let returnElement = <HTMLDivElement>(document.createElement('div'));
-        returnElement.className = 'post-header-line2';
-
-        returnElement.appendChild(this.renderTitle(post.title))
-
-        return returnElement
-
-    }
-
-    private renderButtons(post: Submission) {
-
-    }
-
-    private renderVotes(score: number, hasUpvoted: boolean, hasDownvoted: boolean): HTMLDivElement {
-
-        let returnElement = <HTMLDivElement>(document.createElement('div'));
-        returnElement.className = 'votes';
-
-        // Generate upvote element
-
-        let upvoteImage = <HTMLImageElement>(document.createElement('img'));
-        upvoteImage.className = 'upvote-button'
-        upvoteImage.src = upvoteImagePath;
-        upvoteImage.width = 35;
-
-        // Generate text div
-
-        let voteCounterDiv = <HTMLDivElement>(document.createElement('div'));
-        voteCounterDiv.className = 'vote-counter';
-        voteCounterDiv.innerText = score.toString();
-
-        // Generate downvote element
-
-        let downvoteImage = <HTMLImageElement>(document.createElement('img'));
-        downvoteImage.className = 'downvote-button'
-        downvoteImage.src = downvoteImagePath;
-        downvoteImage.width = 35;
-
-        // Append children
-
-        returnElement.appendChild(upvoteImage);
-        returnElement.appendChild(voteCounterDiv);
-        returnElement.appendChild(downvoteImage);
-
-        return returnElement;
-
-    }
-
-    private renderTitle(title: string) {
-
-        let returnElement = <HTMLSpanElement>(document.createElement('span'));
-        returnElement.className = 'post-title';
-
-        returnElement.innerText = title;
-
-        return returnElement;
     }
 
 }
